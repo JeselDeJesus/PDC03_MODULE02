0eb87bfeac66e5889788a6d107fe862a5f70e662
JeselDeJesus <dejesus.jeselaurvic@auf.edu.ph>
2023-03-03 10:15:30 +0800

    First commit

---
+++ PDC03_MODULE02/PDC03_MODULE02/Activity5.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
+
+namespace PDC03_MODULE02
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Activity5 : ContentPage
+	{ 
+        ObservableCollection<employee> employees = new ObservableCollection<employee>();
+
+		public ObservableCollection<employee> Employees { get { return employees; } }
+	
+		public Activity5 ()
+		{
+			InitializeComponent ();
+
+			{
+				lst.ItemsSource= employees;
+
+				employees.Add(new employee { DisplayName = "Juan Dela Cruz", Position = "President", profileimage="img1.jpg" });
+				employees.Add(new employee { DisplayName = "Peter", Position = "Vice President", profileimage = "img2.jpg" });
+				employees.Add(new employee { DisplayName = "Mercy", Position = "Secretary", profileimage = "img3.jpg" });
+			}
+		}
+	}
+}
